609f8006835f5ac6374aaf41ee00416367180cb4
eligoze <eligoze75@gmail.com>
2026-01-13 17:22:08 -0800

    Added test_get_unary.py

--- tests/unit/test_get_unary.py
+++ tests/unit/test_get_unary.py
@@ -8,24 +8,40 @@ module.
 """
 
 from autoeda.eda import get_unary
-import pandas as pd
-import sys
-import os
+import pytest
+
+# import sys
+# import os
 
 
 def test_single_unary_col(sample_df):
     """
-    Test that get_unary works as expected.
+    Test that get_unary works as expected with default params.
     """
-    out = get_unary(sample_df, threshold=0.75, dropna=False)
-    expected_out = list(["has_car_loan","account_type"])
+    out = get_unary(sample_df)
+    expected_out = list(["has_car_loan", "account_type"])
     assert out == expected_out, f"Expected {expected_out} but got {out}"
 
 
-def test_null_cols(sample_df):
+def test_not_consider_null_cols(sample_df):
     """
-    Test that get_unary works as expected.
+    Test that get_unary works as expected. With dropna=True
     """
-    out = get_unary(sample_df, threshold=0.75, dropna=False)
-    expected_out = list(["has_car_loan","account_type"])
+    out = get_unary(sample_df, dropna=True)
+    expected_out = list(["account_type"])
     assert out == expected_out, f"Expected {expected_out} but got {out}"
+
+
+def test_no_unary_cols(sample_df):
+    """
+    Test that get_unary works as expected. With dropna=True and
+    no unary cols.
+    """
+    out = get_unary(sample_df, threshold=0.9, dropna=True)
+    expected_out = list()
+    assert out == expected_out, f"Expected {expected_out} but got {out}"
+
+
+def test_wrong_input_type_error():
+    with pytest.raises(TypeError, match="df must be a pandas DataFrame"):
+        get_unary("this is not a dataframe")
